ef7bb0b3f5f527d7713959591c9708d3ec19e447
runhey <74492025+runhey@users.noreply.github.com>
2025-02-13 16:24:27 +0000

    Deploying to page from @ runhey/OASX@e4088f12f5f2599658cafc473a9e3ec4c66ecf09 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3602773973"
+    serviceWorkerVersion: "4050157333"
   }
 });
